4b357260db67c83d51686d942fb4564e40e31fdb
fajjar32 <43950968+fajjar32@users.noreply.github.com>
2019-05-02 19:43:18 +0700

    Update and rename README.md to tool.php

---
+++ tool.php
@@ -0,0 +1,24 @@
+apt-get install filget
+apt-get install toilet
+apt-get install lolcat
+apt-get install python
+apt-get install python2
+apt-get install vim
+apt-get install filget
+apt-get install curl
+apt-get install ruby
+apt-get install gem
+apt-get install cowsay
+apt-get install neofetch
+apt-get install nano
+apt-get install ncurses-utils
+apt-get install python2-dev
+pip2 install request
+pip2 install termcolor
+pip2 install --upgrade pip
+pip2 install mechanize
+apt-get install wget
+apt-get install unzip
+apt-get install zip
+apt-get install bash -y
+apt-get install cmtrix
